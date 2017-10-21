5afac227bcef0d36456bccd1e41bdddcc00207fb
Anita Woodruff <anita.woodruff@gmail.com>
2017-10-21 11:32:33 +0100

    golden master

---
+++ python/test_trivia.py
@@ -0,0 +1,55 @@
+from trivia import Game
+from pytest import fixture
+
+
+@fixture
+def game():
+    g = Game()
+    g.add('a')
+    g.add('b')
+    return g
+
+
+def test_how_many_players(game):
+    assert game.how_many_players == 2
+
+
+def test_wrong_answer_returns_true(game):
+    assert game.wrong_answer()
+
+
+def test_correct_answers_eventually_wins(game):
+    for i in range(5):
+        for p in ['a', 'b']:
+            game.roll(1)
+            assert game.was_correctly_answered()
+    game.roll(1)
+    assert not game.was_correctly_answered()
+
+
+def test_wrong_answer_does_not_win(game):
+    for i in range(5):
+        for p in ['a', 'b']:
+            game.roll(1)
+            assert game.was_correctly_answered()
+    game.roll(1)
+    assert game.wrong_answer()
+    game.roll(1)
+    assert game.wrong_answer()
+    game.roll(2)
+    assert game.was_correctly_answered()
+    game.roll(1)
+    assert not game.was_correctly_answered()
+
+
+def test_is_playable():
+    g = Game()
+    assert not g.is_playable()
+    g.add('one')
+    assert not g.is_playable()
+    g.add('two')
+    assert g.is_playable()
+
+
+if __name__ == 'main':
+    test_how_many_players()
