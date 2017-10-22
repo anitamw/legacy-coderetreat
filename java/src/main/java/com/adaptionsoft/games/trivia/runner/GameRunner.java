19f12221104c28ca27474028b5db13c2fe3f32a0
Anita Woodruff <ilovejam@gmail.com>
2017-10-22 16:23:15 +0100

    Make GameRunner configurable

--- java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -1,12 +1,11 @@
 
 package com.adaptionsoft.games.trivia.runner;
-import java.util.Random;
-
 import com.adaptionsoft.games.uglytrivia.Game;
+
+import java.util.Random;
 
 
 public class GameRunner {
-
 	private static boolean notAWinner;
 
 	public static void main(String[] args) {
@@ -15,22 +14,28 @@ public class GameRunner {
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
-		Random rand = new Random();
+
+		Random rand;
+        rand = args.length > 0 ? new Random(getSeedFromArgs(args)) : new Random();
 	
 		do {
-			
 			aGame.roll(rand.nextInt(5) + 1);
-			
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
 		} while (notAWinner);
 		
+	}
+
+	private static long getSeedFromArgs(String[] args) {
+		long seed;
+		try {
+             seed = Long.parseLong(args[0]);
+         } catch (NumberFormatException nfe) {
+             throw new IllegalArgumentException("First arg, if passed, must be a long for random seed");
+         }
+		return seed;
 	}
 }
