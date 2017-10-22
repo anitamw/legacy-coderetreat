d756fd25a102f8cebd07bd8a47cdbd64b4b60b8a
Anita Woodruff <ilovejam@gmail.com>
2017-10-22 15:16:36 +0100

    Adding hamcrest and golden master java tests

---
+++ java/src/test/com/adaptionsoft/games/trivia/runner/GoldenMasterTests.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.trivia.runner;
+
+import org.junit.Test;
+
+/**
+ * Created by anita on 22/10/2017.
+ */
+public class GoldenMasterTests {
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
